6c49a8d4d61125ef25fabf6409b4d116a2d1f5d1
Jon <jlemaitre@theatomgroup.com>
2015-11-05 15:40:39 -0500

    some refactoring

--- resources/assets/angular/controllers/main.controller.js
+++ resources/assets/angular/controllers/main.controller.js
@@ -1,11 +1,11 @@
 (function() {
 	angular
-        .module('app')
-	    .controller("MainController", MainController);
+    .module('app')
+    .controller("MainController", MainController);
 
     MainController.$inject = ['ApplicationState'];
 
     function MainController(ApplicationState) {
         this.vm = ApplicationState;
     }
-})();
+})()8dbc91f7e62c1df80259dfd7124be19f875cb2b2;
